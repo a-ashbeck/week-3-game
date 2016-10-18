f52b411d216adad3f8534951c67b833ab0f6006c
Alan <a-ashbeck@northwestern.edu>
2016-10-18 10:45:36 -0500

    Add font and additional logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,34 @@ var hangman = {
     chances: 12,
     wins: 0,
     losses: 0,
-    wordBank: ['cowboy', 'western', 'wildbill', 'billythekid', 'winchester'],
+    wordBank: [
+            'cowboy', 'western', 'wildbill', 'billythekid', 'winchester',
+            'horse', 'revolver', 'saloon', 'saddle', 'tombstone', 'noose',
+            'gallows', 'boots', 'hat', 'ranch', 'cattle', 'mule'
+    ],
+    idList: [
+            'wins', 'losses', 'chances', 'letters-used',
+            'guess-word', 'win-lose'
+    ],
 
-    gameDisplay: function () {
-        document.getElementById('game').innerHTML =
-            '<div id="wins"></div>' +
-            '<div id="losses"></div>' +
-            '<div id="chances"></div>' +
-            '<div id="letters-used"></div>' +
-            '<div id="guess-word"></div>' +
-            '<div id="win-lose"></div>';
+    // Set the divs and their IDs
+    gameDisplay: function() {
+        var displayDivs = document.getElementById('game');
+        for (var i = 0; i < hangman.idList.length; i++) {
+          var createDiv = document.createElement('div');
+          createDiv.innerHTML = '<div id="' + hangman.idList[i] + '"></p>';
+          displayDivs.appendChild(createDiv);
+        };
+        document.getElementById('win-lose').innerHTML = '';
     },
-    styleSpace: function (placeholder) {
+
+    // Displays the blank lines for the word to be guessed
+    blankLines: function(placeholder) {
         document.getElementById('guess-word').innerHTML = placeholder;
     },
-    stats: function (wins, losses, numIncorrectGuesses, lettersUsed) {
+
+    // Stats display functions
+    stats: function(wins, losses, numIncorrectGuesses, lettersUsed) {
         document.getElementById('wins').innerHTML =
             '<p>Wins: ' + wins +'</p>';
         document.getElementById('losses').innerHTML =
@@ -26,17 +39,17 @@ var hangman = {
         document.getElementById('letters-used').innerHTML =
             '<p>Letters Used so far: ' + lettersUsed.join(' ') + '</p>';
     },
-    endGameWin: function () {
+
+    // End Game display functions
+    endGameWin: function() {
         document.getElementById('win-lose').innerHTML =
             '<h2>YOU WIN!</h2>';
-        document.onkeyup = function () {
-            hangman.gameDisplay();
-            start();
-        };
     },
-    endGameLose: function () {
+    endGameLose: function() {
         document.getElementById('win-lose').innerHTML =
             '<h2>YOU LOSE!</h2>';
+    },
+    restart: function() {
         document.onkeyup = function () {
             hangman.gameDisplay();
             start();
@@ -54,39 +67,51 @@ function start() {
         lettersUsed = [],
         numIncorrectGuesses = 0;
 
-    // write blank word
+    // Create letter blanks in placeholderWord array
     for (var i = 0; i < randomWord.length; i++) {
         placeholderWord.push('__');
-    }
-    hangman.styleSpace(placeholderWord.join('  '));
 
+    }
+    hangman.blankLines(placeholderWord.join(' '));
+
+    // Display game stats
     hangman.stats(hangman.wins, hangman.losses, numIncorrectGuesses, lettersUsed);
 
+    // Game play action
     document.onkeyup = function (event) {
-        var input = String.fromCharCode(event.keyCode).toLowerCase();
+        var input = String.fromCharCode(event.keyCode).toLowerCase(),
+            letters = /^[A-Za-z]+$/;
 
-        if (randomWord.indexOf(input) !== -1) {
-            for (var i = 0; i < randomWord.length; i++) {
-                if (randomWord[i] === input) {
-                    placeholderWord[i] = input;
+        // If statement to determine if key press is a letter or not
+        if(input.match(letters)) {
+            // Code block to swap out correct guesses and log incorrect guesses
+            if (randomWord.indexOf(input) !== -1) {
+                for (var i = 0; i < randomWord.length; i++) {
+                    if (randomWord[i] === input) {
+                        placeholderWord[i] = input.toUpperCase();
+                    };
                 };
+                hangman.blankLines(placeholderWord.join(' '));
+            } else if (lettersUsed.indexOf(input) === -1) {
+                lettersUsed.push(input);
+                numIncorrectGuesses++;
             };
-            hangman.styleSpace(placeholderWord.join('  '));
-        } else if (lettersUsed.indexOf(input) === -1) {
-            lettersUsed.push(input);
-            numIncorrectGuesses++;
-        };
 
-        hangman.stats(hangman.wins, hangman.losses, numIncorrectGuesses, lettersUsed);
+            // Updated stats ouput
+            hangman.stats(hangman.wins, hangman.losses, numIncorrectGuesses, lettersUsed);
 
-        if (placeholderWord.join('').toLowerCase() === randomWord) {
-            hangman.wins++;
-            hangman.endGameWin();
-        };
+            // End game win/lose if statements
+            if (placeholderWord.join('').toLowerCase() === randomWord) {
+                hangman.wins++;
+                hangman.endGameWin();
+                hangman.restart();
+            };
 
-        if ((numIncorrectGuesses) >= hangman.chances) {
-            hangman.losses++;
-            hangman.endGameLose();
+            if ((numIncorrectGuesses) >= hangman.chances) {
+                hangman.losses++;
+                hangman.endGameLose();
+                hangman.restart();
+            };
         };
     };
 }
